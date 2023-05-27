ed524af8545107dadb6bcc24b88d8afc535bf675
Sajan <hideforyear@gmail.com>
2023-05-27 05:38:14 -0700

    env changed

--- app/api/botreq/supportData.js
+++ app/api/botreq/supportData.js
@@ -1,7 +1,16 @@
 const Data = {
-  translatorAuth :process.env.TRANSLATOR_KEY,
-  GPT_OrgId: process.env.GPT_ORG_KEY,
-  GPT_ApiSecretKey : process.env.GPT_API_KEY
+  translatorAuth :'a_UuncMqFc3xKshtL0EEQiiEJaMM1EsXgI6VZgxK8haEMoDo86yQVH7mFEbgVMTIAusyES2ZevsCmuqZuo',
+  GPT_OrgId: 'org-yk6WpjszPzLmXdqGT5ew08tz',
+  GPT_ApiSecretKey : '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'
 }
+
+/*
+TRANSLATOR_KEY =a_UuncMqFc3xKshtL0EEQiiEJaMM1EsXgI6VZgxK8haEMoDo86yQVH7mFEbgVMTIAusyES2ZevsCmuqZuo
+GPT_ORG_KEY =org-yk6WpjszPzLmXdqGT5ew08tz
+GPT_API_KEY =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
+NEXT_PUBLIC_GUPUTA_KEY =iHaveNoIdeaMyApiSecurity2004
+GUPUTA_KEY =iHaveNoIdeaMyApiSecurity2004
+
+*/
 
 export default Data
